beb0803d16d7fd8ec57e62f9c485c02e0f1fb083
David Alexander <davoo294@gmail.com>
2019-03-18 16:56:21 +0300

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -31,7 +31,7 @@ namespace GradeBook.GradeBooks
                 return 'D';
             else return 'F';
 
-            return base.GetLetterGrade(averageGrade);
+            
         }
     }
 }
