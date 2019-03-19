4a564a459563f2464ba92bdc9ee34c4a8574dd94
David Alexander <davoo294@gmail.com>
2019-03-19 13:11:52 +0300

    Update StartingUserInterface.cs

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -34,12 +34,23 @@ namespace GradeBook.UserInterfaces
         public static void CreateCommand(string command)
         {
             var parts = command.Split(' ');
-            if (parts.Length != 2)
+            if (parts.Length != 3)
             {
-                Console.WriteLine("Command not valid, Create requires a name.");
+                Console.WriteLine("Command not valid, Create requires a name and type of gradebook.");
                 return;
             }
             var name = parts[1];
+            var type = parts[2].ToLower();
+            BaseGradeBook gradebook;
+            if (type == "standard")
+                gradebook = new StandardGradeBook(name);
+            else if (type == "ranked") ;
+            else
+            {
+                Console.WriteLine("{0} is not a suppoted type of gradebook, please try again.", type);
+                return;
+            }
+                gradebook = new RankedGradeBook(name);
             BaseGradeBook gradeBook = new BaseGradeBook(name);
             Console.WriteLine("Created gradebook {0}.", name);
             GradeBookUserInterface.CommandLoop(gradeBook);
@@ -67,13 +78,14 @@ namespace GradeBook.UserInterfaces
             Console.WriteLine();
             Console.WriteLine("GradeBook accepts the following commands:");
             Console.WriteLine();
-            Console.WriteLine("Create 'Name' - Creates a new gradebook where 'Name' is the name of the gradebook.");
+            Console.WriteLine("Create 'Name' - Creates a new gradebook where 'Name' is the name of the gradebook and 'Type' is what type of grading it should use.");
             Console.WriteLine();
             Console.WriteLine("Load 'Name' - Loads the gradebook with the provided 'Name'.");
             Console.WriteLine();
             Console.WriteLine("Help - Displays all accepted commands.");
             Console.WriteLine();
             Console.WriteLine("Quit - Exits the application");
+            
         }
     }
 }
